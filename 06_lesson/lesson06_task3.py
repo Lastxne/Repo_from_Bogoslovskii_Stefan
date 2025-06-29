a6c4a114ce3c6fe0e77e7413331e52760113d8cd
Lastxne <filinstelaner@mail.ru>
2025-06-29 17:36:02 +0300

    lesson-six: fix

--- 06_lesson/lesson06_task3.py
+++ 06_lesson/lesson06_task3.py
@@ -1,21 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome()
+url = "https://bonigarcia.dev/selenium-webdriver-java/loading-images.html"
+driver.get(url)
 
-try:
-    driver.get(
-        "https://bonigarcia.dev/selenium-webdriver-java/loading-images.html"
-    )
+WebDriverWait(driver, 10).until(
+    EC.text_to_be_present_in_element((By.ID, "text"), "Done!")
+)
 
-    def img_has_src(d):
-        el = d.find_element(By.ID, "award")
-        return el if el.get_attribute("src") else False
+img = WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.ID, "award"))
+)
+print(img.get_attribute("src"))
 
-    award_img = WebDriverWait(driver, 15).until(img_has_src)
-
-    print(award_img.get_attribute("src"))
-
-finally:
-    driver.quit()
+driver.quit()
