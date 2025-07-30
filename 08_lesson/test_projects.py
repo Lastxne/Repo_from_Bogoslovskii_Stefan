fa3a5b321d4a381b83a8c788a09164f38302ded5
Lastxne <filinstelaner@mail.ru>
2025-07-31 02:05:59 +0300

    lesson-eight second try

--- 08_lesson/test_projects.py
+++ 08_lesson/test_projects.py
@@ -19,3 +19,16 @@ def test_get_project(api, created_project_id):
     resp = api.get_project(created_project_id)
     assert resp.status_code == 200
     assert resp.json()["title"] == "Это не то что вы подумали"
+
+
+@pytest.mark.positive
+def test_create_project(api):
+    data = {
+        "title": "Тестовый проект",
+        "users": {
+            "982d0603-249a-4b4e-a0c0-06a208f7a820": "admin"
+        }
+    }
+    resp = api.create_project(data)
+    assert resp.status_code == 201
+    assert "id" in resp.json()
